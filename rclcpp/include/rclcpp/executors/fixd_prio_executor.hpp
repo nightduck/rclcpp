180e44700450bd5182a8189d2fb95b0d96163a0d
Oren Bell <oren.bell@wustl.edu>
2021-10-24 21:42:50 +0000

    Added header for fixed priority executor

---
+++ rclcpp/include/rclcpp/executors/fixd_prio_executor.hpp
@@ -0,0 +1,304 @@
+// Copyright 2014 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RCLCPP__EXECUTORS__FIXED_PRIO_EXECUTOR_HPP_
+#define RCLCPP__EXECUTORS__FIXED_PRIO_EXECUTOR_HPP_
+
+#include <chrono>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <set>
+#include <thread>
+#include <type_traits>
+#include <unordered_map>
+
+#include "rclcpp/executor.hpp"
+#include "rclcpp/macros.hpp"
+#include "rclcpp/memory_strategies.hpp"
+#include "rclcpp/visibility_control.hpp"
+#include "rclcpp/executors/static_single_threaded_executor.hpp"
+
+namespace rclcpp
+{
+
+class CBG_Work {
+    std::mutex mux;
+    std::condition_variable cond;
+    std::thread * my_thread;
+
+    int priority;
+
+    std::shared_ptr<rclcpp::AnyExecutable> running;
+
+    // TODO: In future, this will be rbtree of min-max heaps, to enforce queue sizes on subs
+    std::priority_queue<std::pair<int,std::shared_ptr<rclcpp::AnyExecutable>>> heap;
+
+    // TODO: Modify this when changing to rbtree of min-max heaps
+    // Put exec in heap, and return true if success
+    bool add_work(rclcpp::AnyExecutable& exec, int prio) {
+        heap.emplace(std::pair<int, std::shared_ptr<rclcpp::AnyExecutable>>
+                (prio, std::make_shared<rclcpp::AnyExecutable>(exec)));
+
+        priority = std::max(prio, priority);
+        return true;
+    }
+
+    // Replace running with most urgent task in heap. Update priority
+    std::shared_ptr<rclcpp::AnyExecutable> get_work() {
+      if (heap.empty()) {
+        running.reset();
+        priority = -1;
+        return running;
+      }
+
+      auto ret = heap.top();
+      heap.pop();
+      priority = ret.first;
+      running = ret.second;
+      return running;
+    }
+};
+
+namespace executors
+{
+
+// TODO: Enforce Adaptor uses correct parameters in its own template: https://www.informit.com/articles/article.aspx?p=376878
+template<
+  typename Compare  = std::less<rclcpp::AnyExecutable> 
+>
+class FixedPrioExecutor : public StaticSingleThreadedExecutor
+{
+public:
+  RCLCPP_SMART_PTR_DEFINITIONS(FixedPrioExecutor)
+
+  /// Constructor for FixedPrioExecutor.
+  /**
+   * For the yield_before_execute option, when true std::this_thread::yield()
+   * will be called after acquiring work (as an AnyExecutable) and
+   * releasing the spinning lock, but before executing the work.
+   * This is useful for reproducing some bugs related to taking work more than
+   * once.
+   *
+   * \param options common options for all executors
+   * \param number_of_threads number of threads to have in the thread pool,
+   *   the default 0 will use the number of cpu cores found instead
+   * \param yield_before_execute if true std::this_thread::yield() is called
+   * \param timeout maximum time to wait
+   */
+  RCLCPP_PUBLIC
+  FixedPrioExecutor(
+    rclcpp::ExecutorOptions & options = rclcpp::ExecutorOptions(),
+    size_t number_of_threads = 0,
+    bool yield_before_execute = false,
+    std::chrono::nanoseconds timeout = std::chrono::nanoseconds(-1));
+
+  RCLCPP_PUBLIC
+  virtual ~FixedPrioExecutor();
+  
+  /// Fixed priority executor implementation of spin all
+  /**
+   * This non-blocking function will execute entities until
+   * timeout or all work has completed with no work left
+   * available.
+   *
+   * Example:
+   *   while(condition) {
+   *     spin_all();
+   *     sleep(); // User should have some sync work or
+   *              // sleep to avoid a 100% CPU usage
+   *   }
+   */
+  RCLCPP_PUBLIC
+  void
+  spin_all(std::chrono::nanoseconds max_duration) override;
+
+  RCLCPP_PUBLIC
+  size_t
+  get_number_of_threads();
+
+protected:
+  /**
+   * @brief Prefetches messages for ready executables and assigns them to the relevant thread
+   * @param spin_once if true executes only the first ready executable.
+   * @return true if any executable was ready.
+   */
+  RCLCPP_PUBLIC
+  bool
+  execute_ready_executables(bool spin_once = false);
+
+  RCLCPP_PUBLIC
+  void
+  run(size_t this_thread_number);
+
+private:
+  RCLCPP_DISABLE_COPY(GenericExecutor)
+
+  // // TODO: Custom allocator here to avoid dynamic memory operations?
+  // typedef std::queue<rclcpp::AnyExecutable>
+  //   ReleasedWorkQueue;
+
+  // ReleasedWorkQueue
+  // released_work_ RCPPUTILS_TSA_GUARDED_BY(mutex_);
+
+  // // TODO: This just hide the base member, which will still be used by all methods not overriden
+  // /// The memory strategy: an interface for handling user-defined memory allocation strategies.
+  // memory_strategies::allocator_memory_strategy::PrefetchMemoryStrategy<std::allocator<void>>
+  // memory_strategy_ RCPPUTILS_TSA_PT_GUARDED_BY(mutex_);
+
+  std::mutex wait_mutex_;
+  size_t number_of_threads_;
+  bool yield_before_execute_;
+  std::chrono::nanoseconds next_exec_timeout_;
+};
+
+
+//// Implementation
+// BUG: Preliminary value for number_of_threads is passed to MemStrat. It would need to be corrected
+template<class MemStrat>
+GenericExecutor<MemStrat>::GenericExecutor(
+  const rclcpp::Context::SharedPtr context,
+  size_t max_conditions,
+  size_t number_of_threads,
+  bool yield_before_execute,
+  std::chrono::nanoseconds next_exec_timeout)
+: rclcpp::Executor(rclcpp::ExecutorOptions(std::make_shared<MemStrat>(number_of_threads), context,
+                                            max_conditions)),
+  yield_before_execute_(yield_before_execute),
+  next_exec_timeout_(next_exec_timeout)
+{
+  number_of_threads_ = number_of_threads ? number_of_threads : std::thread::hardware_concurrency();
+  if (number_of_threads_ == 0) {
+    number_of_threads_ = 1;
+  }
+}
+
+template<class MemStrat>
+GenericExecutor<MemStrat>::GenericExecutor(
+  size_t number_of_threads,
+  bool yield_before_execute,
+  std::chrono::nanoseconds next_exec_timeout)
+: GenericExecutor<MemStrat>(
+      rclcpp::contexts::get_global_default_context(),
+      0, number_of_threads, yield_before_execute, next_exec_timeout)
+{}
+
+template<class MemStrat>
+GenericExecutor<MemStrat>::~GenericExecutor() {}
+
+// This method is defined as an example. It will function as a decentralized non-preemptive
+// scheduler. 
+template<class MemStrat>
+void
+GenericExecutor<MemStrat>::spin()
+{
+  if (spinning.exchange(true)) {
+    throw std::runtime_error("spin() called while already spinning");
+  }
+  RCPPUTILS_SCOPE_EXIT(this->spinning.store(false); );
+  std::vector<std::thread> threads;
+  size_t thread_id = 0;
+  {
+    std::lock_guard wait_lock{wait_mutex_};
+    for (; thread_id < number_of_threads_ - 1; ++thread_id) {
+      auto func = std::bind(&GenericExecutor::run, this, thread_id);
+      threads.emplace_back(func);
+    }
+  }
+
+  run(thread_id);
+  for (auto & thread : threads) {
+    thread.join();
+  }
+}
+
+template<class MemStrat>
+size_t
+GenericExecutor<MemStrat>::get_number_of_threads()
+{
+  return number_of_threads_;
+}
+
+template<class MemStrat>
+void
+GenericExecutor<MemStrat>::wait_for_work(std::chrono::nanoseconds timeout)
+{
+  // TODO: Make atomic
+  Executor::wait_for_work(timeout);
+  std::static_pointer_cast<MemStrat>(memory_strategy_)->collect_work(weak_groups_to_nodes_);
+}
+
+template<class MemStrat>
+bool
+GenericExecutor<MemStrat>::get_next_ready_executable(
+  AnyExecutable & any_executable, size_t this_thread_number)
+{
+  return std::static_pointer_cast<MemStrat>(memory_strategy_)
+            ->get_next_executable(any_executable, this_thread_number);
+}
+
+template<class MemStrat>
+bool
+GenericExecutor<MemStrat>::get_next_executable(AnyExecutable & any_executable,
+        std::chrono::nanoseconds timeout, size_t this_thread_number)
+{
+  bool success = false;
+  // Check to see if there are any subscriptions or timers needing service
+  // TODO(wjwwood): improve run to run efficiency of this function
+  success = get_next_ready_executable(any_executable, this_thread_number);
+  // If there are none
+  if (!success) {
+    // Wait for subscriptions or timers to work on
+    wait_for_work(timeout);
+    if (!spinning.load()) {
+      return false;
+    }
+    // Try again
+    success = get_next_ready_executable(any_executable, this_thread_number);
+  }
+  return success;
+}
+
+template<class MemStrat>
+void
+GenericExecutor<MemStrat>::run(size_t this_thread_number)
+{
+  while (rclcpp::ok(this->context_) && spinning.load()) {
+    rclcpp::AnyExecutable any_exec;
+    {
+      std::lock_guard wait_lock{wait_mutex_};
+      if (!rclcpp::ok(this->context_) || !spinning.load()) {
+        return;
+      }
+      if (!get_next_executable(any_exec, next_exec_timeout_, this_thread_number)) {
+        continue;
+      }
+    }
+    if (yield_before_execute_) {
+      std::this_thread::yield();
+    }
+
+    execute_any_executable(any_exec);
+
+    // Clear the callback_group to prevent the AnyExecutable destructor from
+    // resetting the callback group `can_be_taken_from`
+    any_exec.callback_group.reset();
+  }
+}
+
+
+}  // namespace executors
+}  // namespace rclcpp
+
+#endif  // RCLCPP__EXECUTORS__FIXED_PRIO_EXECUTOR_HPP_
