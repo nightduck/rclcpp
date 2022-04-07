65d797f267a9b80d286849794794b97777704811
Oren Bell <oren.bell@wustl.edu>
2022-04-07 22:27:49 +0000

    Mutual exclusion unit test passing

--- rclcpp/test/rclcpp/executors/test_rt_executors.cpp
+++ rclcpp/test/rclcpp/executors/test_rt_executors.cpp
@@ -17,6 +17,7 @@
 #include <chrono>
 #include <string>
 #include <memory>
+#include <utility>
 
 #include "rclcpp/exceptions.hpp"
 #include "rclcpp/node.hpp"
@@ -27,15 +28,21 @@
 
 
 #include "std_msgs/msg/int32.hpp"
+#include "test_msgs/msg/empty.hpp"
 
 using namespace std::chrono_literals;
 
 class TestRTExecutors : public ::testing::Test
 {
-protected:
+public:
   static void SetUpTestCase()
   {
     rclcpp::init(0, nullptr);
+  }
+
+  static void TearDownTestCase()
+  {
+    rclcpp::shutdown();
   }
 };
 
@@ -153,6 +160,120 @@ constexpr double TOLERANCE = PERIOD / 4.0;
 //   };
 // }
 
-// TODO(nightduck): Test two subscriptions in same cbg don't run at same time
-// TODO(nightduck): Test two subscriptions in diff cbg run at same time
+// Test two subscriptions in same cbg don't run at same time (subs in node 2)
+// Test two subscriptions in diff cbg run at same time (node 1 vs node 2)
+TEST_F(TestRTExecutors, mutual_exclusion) {
+  rclcpp::executors::FixedPrioExecutor executor([](rclcpp::AnyExecutable ae) {return 50;});
+
+  std::shared_ptr<rclcpp::Node> node1 =
+    std::make_shared<rclcpp::Node>("node1");
+  std::shared_ptr<rclcpp::Node> node2 =
+    std::make_shared<rclcpp::Node>("node2");
+
+  auto pub_a = node1->create_publisher<test_msgs::msg::Empty>("topic_a", 10);
+  auto pub_b = node1->create_publisher<test_msgs::msg::Empty>("topic_b", 10);
+
+  // Node1's sub will verify this is zero before triggering node 2's subs, who both increment it.
+  // Node1's sub then verifies the final value is 2, to confirm concurrency between node 1 and 2
+  std::atomic_int race_var;
+  race_var = 0;
+
+  // When node2's subs wake, they also check this variable, increment it, and spin. They then
+  // check the variable didn't change during spin to verify concurrency within ndoe2 didn't occur
+  std::atomic_int threadsafe_var;
+  threadsafe_var = 0;
+
+  std::mutex mux;
+
+  // This will run one, should run concurrently with one instance of above callback
+  auto node1_callback =
+    [&node1, &race_var, &threadsafe_var, &pub_b](test_msgs::msg::Empty::ConstSharedPtr) {
+      ASSERT_EQ(race_var, 0);
+      ASSERT_EQ(threadsafe_var, 0);
+
+      test_msgs::msg::Empty msg;
+      pub_b->publish(msg);
+
+      uint64_t x = 0xDEADBEEF;
+      rclcpp::Time now = node1->now();
+      while (node1->now() < now + rclcpp::Duration::from_nanoseconds(50000000)) {
+        x ^= node1->now().nanoseconds();
+      }
+
+      ASSERT_EQ(race_var, 2);
+    };
+
+  // This will run twice in two subscriptions, should not run concurrently
+  auto node2_callback =
+    [&node2, &race_var, &threadsafe_var, &mux](test_msgs::msg::Empty::ConstSharedPtr) {
+      int start_val;
+      {
+        ASSERT_LT(threadsafe_var, 2);
+
+        std::lock_guard<std::mutex> lock{mux};
+        threadsafe_var++;
+        start_val = threadsafe_var;
+      }
+
+      race_var++;
+
+      uint64_t x = 0xDEADBEEF;
+      rclcpp::Time now = node2->now();
+      while (node2->now() < now + rclcpp::Duration::from_nanoseconds(10000000)) {
+        x ^= node2->now().nanoseconds();
+      }
+
+      ASSERT_EQ(start_val, threadsafe_var);
+    };
+
+  auto sub1 =
+    node1->create_subscription<test_msgs::msg::Empty>("topic_a", 1, std::move(node1_callback));
+  auto sub2 =
+    node2->create_subscription<test_msgs::msg::Empty>("topic_b", 1, std::move(node2_callback));
+  auto sub3 =
+    node2->create_subscription<test_msgs::msg::Empty>("topic_b", 1, std::move(node2_callback));
+
+
+  // Sleep for a short time to verify executor.spin() is going, and didn't throw.
+  executor.add_node(node1);
+  executor.add_node(node2);
+  std::thread spinner([&]() {
+      executor.spin();
+      // std::promise<void> promise;
+      // std::future<void> future = promise.get_future();
+
+      // executor.spin_until_future_complete(future, std::chrono::milliseconds(100));
+    });
+
+  test_msgs::msg::Empty msg;
+  pub_a->publish(msg);
+
+  std::this_thread::sleep_for(100ms);
+  executor.cancel();
+  spinner.join();
+
+  executor.remove_node(node1, true);
+  executor.remove_node(node2, true);
+
+  sub1.reset();
+  sub2.reset();
+  sub3.reset();
+
+  pub_a.reset();
+  pub_b.reset();
+
+  node1.reset();
+  node2.reset();
+
+  ASSERT_EQ(threadsafe_var, 2);
+}
+
 // TODO(nightduck): Test two subscriptions in same reenrant cbg run at same time
+
+TEST_F(TestRTExecutors, priority_checking) {
+  rclcpp::executors::FixedPrioExecutor executor(
+    [](rclcpp::AnyExecutable ae) {
+      auto msg = std::static_pointer_cast<std_msgs::msg::Int32>(ae.data);
+      return msg->data;
+    });
+}
